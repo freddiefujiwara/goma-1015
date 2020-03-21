5d424bf624928c9ba5e9a3f5193c5b2aa23d014f
Fumikazu Fujiwara <github@ze.gs>
2020-03-21 10:19:25 +0900

    add fast-check

--- __tests__/lib/index.spec.ts
+++ __tests__/lib/index.spec.ts
@@ -1,3 +1,5 @@
+import fc from 'fast-check'
+
 import Goma1015 from '../../src/lib/index'
 
 describe('Goma1015', () => {
@@ -5,5 +7,22 @@ describe('Goma1015', () => {
     const g = new Goma1015()
     expect(g).not.toBeNull()
     expect(g).toBeInstanceOf(Goma1015)
+  })
+  it('can open or close', () => {
+    const g = new Goma1015()
+    expect(g.open).toBeDefined()
+    expect(g.close).toBeDefined()
+    expect(g.isOpen).toBeDefined()
+    fc.assert(
+      fc.property(fc.boolean(), b => {
+        if (b) {
+          g.open()
+          expect(g.isOpen()).toBe(true)
+          return
+        }
+        g.close()
+        expect(g.isOpen()).toBe(false)
+      }),
+    )
   })
 })
