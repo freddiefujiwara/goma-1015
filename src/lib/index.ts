aaeb4ea0f18a2df8b21c9ad005d8557812e30f75
Fumikazu Fujiwara <github@ze.gs>
2020-03-23 10:10:05 +0900

    add JSDoc comment

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,18 +6,33 @@ export enum State {
   ON_ACTIVE_BOIL,
   ON_ACTIVE_KEEP,
 }
-
+/** Class GOMA-1015 http://www.sessame.jp/workinggroup/WorkingGroup2/POT_Specification.htm */
 export class Goma1015 {
+  /** to manage state transition */
   private _state: number
+  /** start warning time to calculate water temperature  */
   private _start: number
+  /** to manage water volume */
   private _water: number
+  /** to manage temperature */
   private _temperature: number
+  /**
+   *
+   * Create GOMA-1015
+   *
+   **/
   constructor() {
     this._state = State.OFF
     this._start = 0
     this._water = 0
     this._temperature = 25
   }
+  /**
+   * open the lid
+   *
+   * @return void
+   *
+   **/
   open(): void {
     switch (this._state) {
       case State.OFF:
@@ -33,6 +48,12 @@ export class Goma1015 {
         break
     }
   }
+  /**
+   * close the lid
+   *
+   * @return void
+   *
+   **/
   close(): void {
     switch (this._state) {
       case State.OFF_OPEN:
@@ -46,6 +67,12 @@ export class Goma1015 {
         break
     }
   }
+  /**
+   * plug the outlet
+   *
+   * @return void
+   *
+   **/
   plugIn(): void {
     switch (this._state) {
       case State.OFF:
@@ -59,6 +86,12 @@ export class Goma1015 {
         break
     }
   }
+  /**
+   * Unplug the outlet
+   *
+   * @return void
+   *
+   **/
   plugOff(): void {
     switch (this._state) {
       case State.ON_IDLE:
@@ -74,9 +107,16 @@ export class Goma1015 {
         break
     }
   }
+  /**
+   * check the current state
+   *
+   * @return state id:number
+   *
+   **/
   state(): number {
     switch (this._state) {
       case State.ON_ACTIVE_BOIL:
+        // water is warmed 1.25 degree / sec to boil from 25 degree
         this._temperature = 25 + ((Date.now() - this._start) / 1000) * 1.25
         if (this._temperature >= 100) {
           this._temperature = 100
@@ -84,11 +124,13 @@ export class Goma1015 {
         }
         break
       case State.ON_ACTIVE_KEEP:
+        // it gonna be IDLE it if it doesn't have enough water
         if (this._water < 10) {
           this._state = State.ON_IDLE
         }
         break
       case State.ON_IDLE:
+        // it gonna boil it if it has enough water
         if (this._water >= 10) {
           this._start = Date.now()
           this._state = State.ON_ACTIVE_BOIL
@@ -99,14 +141,33 @@ export class Goma1015 {
     }
     return this._state
   }
+  /**
+   * check the current tempearature
+   *
+   * @return tempearature degree:number
+   *
+   **/
   temperature(): number {
     this.state()
     return this._temperature
   }
+  /**
+   * check the current water volume
+   *
+   * @return water volume ml:number
+   *
+   **/
   water(): number {
     this.state()
     return this._water
   }
+  /**
+   * fill water
+   *
+   * @param water:number - how much water is filling
+   * @return void
+   *
+   **/
   fill(water: number): void {
     if (water < 0) {
       throw new Error(`${JSON.stringify(this)} can't be filled with negative number`)
@@ -119,6 +180,13 @@ export class Goma1015 {
     }
     this._water += water
   }
+  /**
+   * push the dispense button
+   *
+   * @param sec:number - how long pressed
+   * @return the volume of water pouring:number
+   *
+   **/
   dispense(sec: number): number {
     if (sec < 0) {
       throw new Error(`${JSON.stringify(this)} can't be dispensed with negative sec`)
@@ -134,6 +202,11 @@ export class Goma1015 {
     this.state()
     return water - this._water
   }
+  /**
+   * push the reboil button
+   * @return void
+   *
+   **/
   reboil(): void {
     if (this._state !== State.ON_ACTIVE_KEEP) {
       throw new Error(`${JSON.stringify(this)} should be KEEP`)
